5f2fc688d65a84f5d3be7c58e23906bf9daf529a
Nicolas Hohaus <nhohaus@usa.com>
2022-07-05 11:04:35 +0200

    Update QueryProcessor.java

--- src/main/java/com/develogical/QueryProcessor.java
+++ src/main/java/com/develogical/QueryProcessor.java
@@ -12,6 +12,11 @@ public class QueryProcessor {
             return "Nico";
         }
         if (query.toLowerCase().contains("largest")) {
+            String[] a = query.split(" |,");
+        	int num1 = Integer.parseInt(a[8]);
+        	int num2 = Integer.parseInt(a[9]);
+        	
+            return num1 + num2 + "";
             return "20106";
         }
         
